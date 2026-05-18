4f212e9a27247779c6a2e96f8bc9046846ccd882
sawantshambha-commits <sawantshambha@gmail.com>
2026-05-18 20:30:09 +0530

    solved sql queries 11-17

--- EV_marekt.sql
+++ EV_marekt.sql
@@ -78,28 +78,65 @@ from ev_market
 group by brand
 order by Total_Numbers desc;
 
--- Find the price difference between Tesla’s “Model 3” and “Model Y”.
+-- 11 Find the price difference between Tesla’s “Model 3” and “Model Y”.
+with Model_3 as (select model, price_usd from ev_market
+where brand = "Tesla" and model = "Model 3"
+limit 1),
+Model_Y as (select model, price_usd from ev_market
+where brand = "Tesla" and model = "Model Y"
+limit 1)
+select abs(round((Model_3.price_usd - Model_Y.Price_usd), 02)) as diff
+from Model_3, Model_Y;
 
--- List all EVs whose price is above the overall average price.
+-- 12 List all EVs whose price is above the overall average price.
+select * from ev_market
+where price_usd > (select avg(price_usd) from ev_market);
 
 -- 🚀 Advanced-Level (Real-world & Interview Type)
 -- These mimic analytics and decision-making scenarios.
 
--- Rank all EVs by price within each brand using a window function.
+-- 13 Rank all EVs by price within each brand using a window function.
+select brand, model, year, price_usd, 
+dense_rank() over(partition by brand order by price_usd desc) as Ranking
+from ev_market;
 
--- Find the second most expensive EV for each brand.
+-- 14 Find the second most expensive EV for each brand.
+with abc as( select *, dense_rank() over (partition by brand order by price_usd desc) as Ranking
+from ev_market)
+select brand, model, price_usd from abc
+where Ranking = 2;
 
--- Determine which brand has the highest price growth trend between 2022–2025.
+-- 15 Determine which brand has the highest price growth trend between 2022–2025.
+with yrly_avg_price as(
+select brand, year, avg(price_usd) as avg_price from ev_market
+group by brand, year),
+growth_trend as(
+select brand, max(avg_price) - min(avg_price) as growth
+from yrly_avg_price
+group by brand)
+select * from growth_trend
+order by growth desc
+limit 1;
 
--- Create a query to categorize EVs into price segments:
+-- 16 Create a query to categorize EVs into price segments:
+-- “Budget” (< $40,000), “Mid-range” ($40,000–$80,000), “Luxury” (> $80,000)
 
--- “Budget” (< $40,000)
+select brand, model, price_usd,
+case when price_usd < 40000 then "Budget"
+when price_usd < 80000 then "Mid-range"
+else "Luxury"
+end as price_category
+from ev_market
+order by price_category;
 
--- “Mid-range” ($40,000–$80,000)
 
--- “Luxury” (> $80,000)
-
--- Find the brand with the most variants launched after 2023.
+-- 17 Find the brand with the most variants launched after 2023.
+select brand, count(distinct(variant)) as Total_Variant
+from ev_market
+where year > "2023"
+group by brand
+order by Total_Variant desc
+limit 1;
 
 -- Using a subquery, list all EVs whose price is greater than Tesla’s average price.
 
@@ -118,3 +155,7 @@ order by Total_Numbers desc;
 
 -- Generate a report showing brand, model, year, price, and rank (ranked by price descending).
 
+select * from ev_market;
+select brand, max(price_usd) from ev_market
+group by brand
+order by max(price_usd) desc;
